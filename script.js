630796014c64dac9db8701dd9bd1972ec9c89761
Bradley Donahue <donahue.bradley@gmail.com>
2020-12-09 15:59:16 -0500

    Add password validation

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function writePassword() {
 
 }
 
-function generatePassword() {
-  let characters = 'abcdefghijklmnopqrstuvwxyz';
-  let numbers = '01234567890';
-  let special = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+let characters = 'abcdefghijklmnopqrstuvwxyz';
+let numbers = '01234567890';
+let special = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+function generatePassword() {
   let passwordLength = prompt("Enter a password length between 8-128.");
   passwordLength = parseInt(passwordLength, 10);
 
@@ -42,13 +42,58 @@ function generatePassword() {
     charsToUse += special;
   }
 
-  let generated = [];
-  for (let i = 0; i < passwordLength; i++) {
-    let char = charsToUse[Math.floor(Math.random() * charsToUse.length)];
-    generated.push(char);
+  let validPassword = false;
+  let generated;
+  while (!validPassword) {
+    generated = [];
+    for (let i = 0; i < passwordLength; i++) {
+      let char = charsToUse[Math.floor(Math.random() * charsToUse.length)];
+      generated.push(char);
+    }
+    validPassword = isValidPassword(generated, useLowercase, useUppercase, useNumeric, useSpecial);
   }
 
   return generated.join('');
+}
+
+function isValidPassword(password, useLower, useUpper, useNumeric, useSpecial) {
+  if (useLower && !hasLower(password)) {
+    return false;
+  }
+  if (useUpper && !hasUpper(password)) {
+    return false;
+  }
+  if (useNumeric && !hasNumeric(password)) {
+    return false;
+  }
+  if (useSpecial && !hasSpecial(password)) {
+    return false;
+  }
+  return true;
+}
+
+function hasLower(password) {
+  return password.some(letter => {
+    return characters.includes(letter);
+  });
+}
+
+function hasUpper(password) {
+  return password.some(letter => {
+    return characters.toUpperCase().includes(letter);
+  });
+}
+
+function hasNumeric(password) {
+  return password.some(num => {
+    return numbers.includes(num);
+  });
+}
+
+function hasSpecial(password) {
+  return password.some(char => {
+    return special.includes(char);
+  });
 }
 
 // Add event listener to generate button
